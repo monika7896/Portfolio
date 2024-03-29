05ce69e4bc2f7c9ebec1e18fd1ff1c94d69f2f78
monika7896 <monika79yadav@gmail.com>
2024-03-30 00:07:06 +0530

    ok

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,9 +5,10 @@ export const Bio = {
     // "Android Developer",
     // "UI/UX Designer",
     // "Programmer",
+    // eret
   ],
   description:
-    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    "I am a motivated and versatile individual, always eager to take on new challenges . With a passion for learning . I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/monika7896 ",
   resume:
     "https://drive.google.com/file/d/1ihE7HKkpt9IUdLgztWHU5RjSHNnk1NNZ/view?usp=sharing",
